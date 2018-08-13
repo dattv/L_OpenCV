3ad13b985891ddde3a5b6c9a2be85c2df7301e7b
dattv <datthangvan@gmail.com>
2018-08-13 23:38:47 +0900

    first example

---
+++ example.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef _EXAMPLE_H
+#define _EXAMPLE_H
+
+#include "include.h"
+
+void example01();
+
+#endif // !_EXAMPLE_H
+
---
+++ include.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _INCLUDE_H
+#define _INCLUDE_H
+
+#include <stdio.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv/cv.h"
+#include "opencv2/opencv.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/opencv.hpp"
+
+#endif // !_INCLUDE_H
+
