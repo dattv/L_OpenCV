492e52c2bee521cc9047ae95466a687863400a85
dattv <datthangvan@gmail.com>
2018-08-13 23:55:18 +0900

    add comment for first example

--- example.cpp
+++ example.cpp
@@ -2,12 +2,17 @@
 #include "example.h"
 
 int example01() {
-
+	// Read image
 	cv::Mat img = cv::imread("D:/2018_research/L_OpenCV/hamy.jpg", 1);
+	// Check reading image
 	if (img.empty()) return -1;
+	// Set window size to naturally size of image
 	cv::namedWindow("Hamy", cv::WINDOW_AUTOSIZE);
+	// Print image to screen
 	cv::imshow("Hamy", img);
+	// wait for a keystroke 
 	cv::waitKey(0);
+	// deallocation of images
 	cv::destroyWindow("Hamy");
 	return 1;
 }
